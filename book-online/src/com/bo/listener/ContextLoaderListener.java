53ffa6fc88821647455f852d4e824e512f501f7d
mqxu <2501015371@qq.com>
2019-10-05 14:45:24 +0800

    用户，图书参数和监听类的编写

---
+++ book-online/src/com/bo/listener/ContextLoaderListener.java
@@ -0,0 +1,61 @@
+package com.bo.listener;
+
+
+import com.bo.entity.Book;
+import com.bo.entity.User;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@WebListener
+public class ContextLoaderListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("容器启动");
+        //创建并生成用户数据列
+        List<User> userList = new ArrayList<>(3);
+        User[] users = {
+                new User(1,"gjy","123","管爸爸","user1.jpg","江苏南京", LocalDate.of(2018,6,11)),
+                new User(2,"sjw","123","孙儿子","user2.jpg","江苏南京", LocalDate.of(2018,4,4)),
+                new User(3,"dhb","123","戴儿子","user3.jpg","江苏南京", LocalDate.of(2018,8,3)),
+
+        };
+        userList = Arrays.asList(users);
+
+        //创建并生成图书数据列表
+        List<Book> bookList = new ArrayList<>(10);
+        Book[] books = {
+                new Book(1,"漫长婚礼","b1.jpg","111"),
+                new Book(2,"漫长婚礼","b1.jpg","111"),
+                new Book(3,"漫长婚礼","b1.jpg","111"),
+                new Book(4,"漫长婚礼","b1.jpg","111"),
+                new Book(5,"漫长婚礼","b1.jpg","111"),
+                new Book(6,"漫长婚礼","b1.jpg","111"),
+                new Book(7,"漫长婚礼","b1.jpg","111"),
+                new Book(8,"漫长婚礼","b1.jpg","111"),
+                new Book(9,"漫长婚礼","b1.jpg","111"),
+                new Book(10,"漫长婚礼","b1.jpg","111"),
+        };
+        bookList = Arrays.asList(books);
+
+        //获得全局变量
+        ServletContext servletContext = sce.getServletContext();
+        //设置全局变量属性，将用户和图书列表数据记入，整个应用可以共享
+        servletContext.setAttribute("userList",userList);
+        servletContext.setAttribute("bookLIst",bookList);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("容器销毁");
+
+    }
+}
